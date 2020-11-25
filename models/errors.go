1c86fa710d06d3faad6da5afb48613a76bdf7cc5
Christian Loera <chatoloera18@gmail.com>
2020-11-25 15:20:48 -0600

    Made errors a public error

--- models/errors.go
+++ models/errors.go
@@ -1,37 +1,48 @@
 package models
 
 import (
-    "errors"
+    "strings"
 )
+
+type modelError string
 
 var (
     /* Return when a resource cannot be found in database */
-    ErrNotFound = errors.New("models: Resource not found")
+    ErrNotFound modelError = "models: Resource not found"
 
     /* Return when invalid ID is provided */
-    ErrIDInvalid = errors.New("models: ID provided was invalid")
+    ErrIDInvalid modelError = "models: ID provided was invalid"
 
     /* Return when password isn't provided */
-    ErrPasswordRequired = errors.New("models: Password is required")
+    ErrPasswordRequired modelError = "models: Password is required"
 
     /* Return when password length less than 8 */
-    ErrPasswordTooShort = errors.New("models: Password must be at least 8 characters long")
+    ErrPasswordTooShort modelError = "models: Password must be at least 8 characters long"
 
     /* Return when invalid password is used */
-    ErrPasswordIncorrect = errors.New("models: Incorrect password")
+    ErrPasswordIncorrect modelError = "models: Incorrect password"
 
     /* Return when an email address isn't provided */
-    ErrEmailRequired = errors.New("models: Email address is required")
+    ErrEmailRequired modelError = "models: Email address is required"
 
     /* Return when an email address is invalid */
-    ErrEmailInvalid = errors.New("models: Email address is not valid")
+    ErrEmailInvalid modelError = "models: Email address is not valid"
 
     /* Return when an provided email is already taken */
-    ErrEmailTaken = errors.New("models: Email address is already taken")
+    ErrEmailTaken modelError = "models: Email address is already taken"
 
     /* Return when a remember token hash isn't provided */
-    ErrRememberRequired = errors.New("models: Remember token is required")
+    ErrRememberRequired modelError = "models: Remember token is required"
 
     /* Return when remember token isn't at least 32 bytes */
-    ErrRememberTooShort = errors.New("models: Remember token must be at least 32 bytes")
+    ErrRememberTooShort modelError = "models: Remember token must be at least 32 bytes"
 )
+
+func (err modelError) Error() string {
+    return string(err)
+}
+
+func (err modelError) Public() string {
+    str := strings.Replace(string(err), "models: ", "", 1)
+    return str
+}
