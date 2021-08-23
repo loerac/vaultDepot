d53ecd378c218e4d8bdb05c594ed44b6a884ae3d
loerac <chatoloera@gmail.com>
2021-08-23 18:53:20 -0500

    Updated user inputs
    * Created a function to get the user input rather than duplicating the code
    * Allow three attempts to enter a valid password or secret key

--- models/utils.go
+++ models/utils.go
@@ -1,14 +1,28 @@
 package models
 
 import (
-	"fmt"
-	"strings"
+    "fmt"
+    "log"
+    "strings"
     "time"
 
     "github.com/jinzhu/gorm"
 
-	"golang.org/x/crypto/ssh/terminal"
+    "golang.org/x/crypto/ssh/terminal"
 )
+
+func UserInput(prompt string) (string, error) {
+	fmt.Printf("%s: ", prompt)
+	byte_input, err := terminal.ReadPassword(0)
+	if err != nil {
+        return "", err
+	}
+	user_input := string(byte_input)
+    user_input = strings.TrimSpace(user_input)
+    fmt.Println()
+
+    return user_input, nil
+}
 
 /**
  * @brief:  Ask user for input and hide the input
@@ -18,32 +32,28 @@ import (
  * @return: input on success, else error
  **/
 func HiddenInput(msg string) string {
-    for {
-        fmt.Printf("Enter %s: ", msg)
-        value, err := terminal.ReadPassword(0)
+    attempts := 3
+    for attempts > 0 {
+        input_1, err := UserInput("Enter " + msg)
         if err != nil {
-            panic(err)
+            log.Fatal(err)
         }
-        input_1 := string(value)
-        input_1 = strings.TrimSpace(input_1)
-        fmt.Println("")
 
-        fmt.Printf("Re-enter %s: ", msg)
-        value, err = terminal.ReadPassword(0)
+        input_2, err := UserInput("Re-enter " + msg)
         if err != nil {
-            panic(err)
+            log.Fatal(err)
         }
-        input_2 := string(value)
-        input_2 = strings.TrimSpace(input_2)
-        fmt.Println("")
 
-        if input_1 != input_2 {
-            fmt.Printf("%s don't match\n", msg)
-            time.Sleep(time.Second)
-        } else {
+        if input_1 == input_2 {
             return input_1
         }
+
+        attempts--
+        fmt.Printf("%s don't match\n", msg)
+        time.Sleep(time.Second)
     }
+
+    return ""
 }
 
 /**
